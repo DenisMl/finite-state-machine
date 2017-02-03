b3a4163a81093315aef44f019f59a3686ec16c93
Denis Molodianovich <denisdm95poker@gmail.com>
2017-02-04 01:57:06 +0200

    Finite-state-machine/ Implement: constructor, getState, changeState, trigger methods

--- src/fsm.js
+++ src/fsm.js
@@ -3,25 +3,51 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      this.config = JSON.parse(JSON.stringify(config));
+      //console.log("C>" + this.config.initial);
+      if (!config) {
+        throw new Error ("Config isn't passed");
+      }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      //console.log("GS> " + this.config.initial);
+      return this.config.initial;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      if (state in this.config.states) {
+        this.config.initial = state;
+        //console.log("CS> " + this.config.initial);
+        return this;
+      }
+      else {
+        throw new Error ("State isn't exist");
+      }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (event in this.config.states[this.config.initial].transitions) {
+        //console.log(">>" + this.config.initial + "+" + event + "=" + this.config.states[this.config.initial].transitions[event]);
+        this.config.initial = this.config.states[this.config.initial].transitions[event];
+      }
+      else {
+        throw new Error ("Event in current state isn't exist");
+      }
+    }
 
     /**
      * Resets FSM state to initial.
